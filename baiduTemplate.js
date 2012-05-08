be21c5d62f3fa22a1c98cfaf430b60a38f309d0f
wangxiao <1988wangxiao@gmail.com>
2012-05-08 14:07:00 +0800

    修改升级
    去掉baidu._template命名空间
    去掉baidu.template.setTab()方法，支持baidu.template.RIGHT_DELIMITER设置分隔符
    优化函数性能。

--- baiduTemplate.js
+++ baiduTemplate.js
@@ -1,10 +1,8 @@
 /*
-	占用两个命名空间  baidu.template 和 baidu.template_
+	占用命名空间  baidu.template
 	接口调用方式如下
 	baidu.template(tpl);  //return function
 	baidu.template(tpl,data); //return html string
-	baidu._template.setTab(left,right);
-	baidu._template中有部分方法
 */
 
 (function(){
@@ -21,26 +19,23 @@
 			new Function("obj",
 				"var p=[];"+"with(obj){p.push('"+analysisStr(str)+"');}return p.join('');"
 			);
-			//有数据则返回HTML字符串，没有数据则返回函数
+		//有数据则返回HTML字符串，没有数据则返回函数
 		return data ? fn( data ) : fn;
 	};
 
-	//取得命名空间 baidu.template和 baidu._template
+	//取得命名空间 baidu.template
 	baidu=window.baidu||{};
 	baidu.template=bt;
-
-	baidu._template={};
-	bt_=baidu._template;
 /*
 	//是否开启调试
 	var debug_flag=false;
-	bt_.debug=function(){
+	bt.debug=function(){
 		debug_flag=true;
 		console.log('\nbaidu template debug is running!\n');
 	}
 */
 	//HTML转义
-	bt_._encodeHTML = function (source) {
+	bt._encodeHTML = function (source) {
 		return String(source)
 				.replace(/&/g,'&amp;')
 				.replace(/</g,'&lt;')
@@ -50,13 +45,13 @@
 	};
 
 	//转义影响正则的字符
-	bt_._encodeReg = function (source) {
+	bt._encodeReg = function (source) {
 		return String(source)
 			.replace(new RegExp("([.*+?^=!:\x24{}()|[\\]\/\\\\])", "g"), '\\\x241');
 	};
 
 	//转义UI UI变量使用在HTML页面标签onclick等事件函数参数中
-	bt_._encodeEventHTML = function (source) {
+	bt._encodeEventHTML = function (source) {
 		return String(source)
 			.replace(/&/g,'&amp;')
 			.replace(/</g,'&lt;')
@@ -72,74 +67,51 @@
 	//缓存
 	var cache = {};
 	
-	//自定义表示，尽量不要含有正则中的表示，如果出现，需要转义
-	var _left='<%';
-	var _right='%>';
-
-	//自定义标签的方法
-	bt_.setTab=function(left,right){
-		//强制转换string 并且 过滤可能影响正则的字符
-		_left=bt_._encodeReg(String(left));
-		_right=bt_._encodeReg(String(right));
-	};
+	//自定义分隔符，不要含有正则中的字符
+	bt.LEFT_DELIMITER=baidu.template.LEFT_DELIMITER||'<%';
+	bt.RIGHT_DELIMITER=baidu.template.RIGHT_DELIMITER||'%>';
 
 	//解析模板字符串
 	var analysisStr=function(str){
 		var temp=String(str);
 		temp=temp
-
-			//把所有换行空格去掉  \r回车符 \t制表符或空格 \n换行符
-			.replace(new RegExp("[\\r\\t\\n]","g"), "");
-
-			//默认支持HTML注释
-			//.replace(new RegExp("<!--[\\s\\S]*?-->","g"),"")
-
+			//把所有换行空格去掉  \r回车符 \t制表符 \n换行符
+			.replace(new RegExp("[\\r\\t\\n]","g"), "")
 			//去掉注释内容  <%* 这里可以任意的注释 *%>
-			.replace(new RegExp(_left+"\\*[\\s\\S]*?\\*"+_right, "g"),"");
-
+			//默认支持HTML注释
+			.replace(new RegExp(bt.LEFT_DELIMITER+"\\*[\\s\\S]*?\\*"+bt.RIGHT_DELIMITER, "g"),"")
 			//对变量后面的分号做容错(包括转义模式 如<%:h=value%>)  <%=value;%> 排除掉函数的情况 <%fun1();%> 排除定义变量情况  <%var val='test';%>
-			.replace(new RegExp("("+_left+":?[hvu]=[^;|"+_right+"]*?);[\\s]*?"+_right,"g"),"$1"+_right);
-
+			.replace(new RegExp("("+bt.LEFT_DELIMITER+":?[hvu]?[\\s]*?=[\\s]*?[^;|"+bt.RIGHT_DELIMITER+"]*?);[\\s]*?"+bt.RIGHT_DELIMITER,"g"),"$1"+bt.RIGHT_DELIMITER)
 			//定义变量，如果没有分号，需要容错  <%var val='test'%>
-			.replace(new RegExp("("+_left+"[\\s]*?var[\\s]*?.*?[\\s]*?[^;])[\\s]*?"+_right,"g"),"$1;"+_right);
-
+			.replace(new RegExp("("+bt.LEFT_DELIMITER+"[\\s]*?var[\\s]*?.*?[\\s]*?[^;])[\\s]*?"+bt.RIGHT_DELIMITER,"g"),"$1;"+bt.RIGHT_DELIMITER)
 			//用来清理会报错的容易产生的无用间隔  如 <%if()%><%{%><%}%><%else%><%{%><%}%>
-			.replace(new RegExp("(?:"+_right+_left+")?[\\s]*?\\{","g"),"{").replace(new RegExp("\\}[\\s]*?(?:"+_right+_left+")?[\\s]*?else\\b(?:"+_right+_left+")?\\{","g"),"}else{");
-
+			.replace(new RegExp("(?:"+bt.RIGHT_DELIMITER+bt.LEFT_DELIMITER+")?[\\s]*?\\{","g"),"{").replace(new RegExp("\\}[\\s]*?(?:"+bt.RIGHT_DELIMITER+bt.LEFT_DELIMITER+")?[\\s]*?else\\b(?:"+bt.RIGHT_DELIMITER+bt.LEFT_DELIMITER+")?\\{","g"),"}else{")
 			//按照 <% 分割为一个个数组，再用 \t 和在一起，相当于将 <% 替换为 \t
 			//将模板按照<%分为一段一段的，再在每段的结尾加入 \t,即用 \t 将每个模板片段前面分隔开
-			.split(_left).join("\t");
-
+			.split(bt.LEFT_DELIMITER).join("\t")
 			//找到 不是\t开头并且以单引号结束的字符串 或者 从%>开始含有不是\t的字符并且以点结束的字符串
-			.replace(new RegExp("((^|"+_right+")[^\\t]*)'","g"), "$1\\r");
-
+			.replace(new RegExp("((^|"+bt.RIGHT_DELIMITER+")[^\\t]*)'","g"), "$1\\r")
 			//找到 \t=任意一个字符%> 替换为 ‘，任意字符,'
 			//即替换简单变量  \t=data%> 替换为 ',data,'
 			//默认转义
-			.replace(new RegExp("\\t=(.*?)"+_right,"g"),"',typeof $1=='undefined'?'':baidu._template._encodeHTML($1),'")
-
+			.replace(new RegExp("\\t=(.*?)"+bt.RIGHT_DELIMITER,"g"),"',typeof $1=='undefined'?'':baidu.template._encodeHTML($1),'")
 			//支持不转义 <%:=value%>
-			.replace(new RegExp("\\t:=(.*?)"+_right,"g"),"',typeof $1=='undefined'?'':$1,'")
-
+			.replace(new RegExp("\\t:=(.*?)"+bt.RIGHT_DELIMITER,"g"),"',typeof $1=='undefined'?'':$1,'")
 			//支持url转义 <%:u=value%>
-			.replace(new RegExp("\\t:u=(.*?)"+_right,"g"),"',typeof $1=='undefined'?'':encodeURIComponent($1),'")
-
+			.replace(new RegExp("\\t:u=(.*?)"+bt.RIGHT_DELIMITER,"g"),"',typeof $1=='undefined'?'':encodeURIComponent($1),'")
 			//支持UI 变量使用在HTML页面标签onclick等事件函数参数中  <%:v=value%>
-			.replace(new RegExp("\\t:v=(.*?)"+_right,"g"),"',typeof $1=='undefined'?'':baidu._template._encodeEventHTML($1),'")
-
+			.replace(new RegExp("\\t:v=(.*?)"+bt.RIGHT_DELIMITER,"g"),"',typeof $1=='undefined'?'':baidu.template._encodeEventHTML($1),'")
 			//将字符串按照 \t 分成为数组，在用'); 将其合并，即替换掉结尾的 \t 为 ');
 			//在if，for等语句前面加上 '); ，形成 ');if  ');for  的形式
-			.split("\t").join("');");
-
+			.split("\t").join("');")
 			//将 %> 替换为p.push('
 			//即去掉结尾符，生成函数中的push方法
 			//如：if(list.length=5){%><h2>',list[4],'</h2>');}
 			//会被替换为 if(list.length=5){p.push('<h2>',list[4],'</h2>');}
-			.split(_right).join("p.push('");
-
+			.split(bt.RIGHT_DELIMITER).join("p.push('")
 			//将 \r 替换为 \
 			.split("\r").join("\\'");
-		
 		return temp;
 	};
+
 })();
